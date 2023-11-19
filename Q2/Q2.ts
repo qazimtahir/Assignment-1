52366bc17e087c283f999f1e0a533cd4e503be54
Muhammad Tahir Iqbal <146485833+qazimtahir@users.noreply.github.com>
2023-11-19 23:12:42 +0500

    Add files via upload

--- Q2/Q2.ts
+++ Q2/Q2.ts
@@ -1,4 +1,9 @@
 let hisName: string = "Sir Jahanzaib Tayyab,";
+
+//results using concatenation
 console.log(
   '"' + "Hello Dear " + hisName + " you are a professional teacher." + '"'
 );
+
+// Same results using Template Literals
+console.log(`"Hello Dear ${hisName} you are a professional teacher."`);
